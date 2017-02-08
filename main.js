c9b60208753ab86ada99c3d7aed77d9eddfcf3f9
Flippylolz <assasin463@gmail.com>
2017-02-08 22:18:32 +0200

    alert added

--- main.js
+++ main.js
@@ -4,23 +4,18 @@
         {
             $(this).closest('.color-block').find('.slider').slideToggle("slow");
         });
+        $('.buyButton').on('click', function()
+        {
+            var price = $(this).closest('.color-block').find('.slider').data('price');
+            var amount = $(this).closest('.color-block').find('#amount').val();
+            var totalPrice = getPrice(price, amount);
+            var message = "Ваш заказ на сумму " + totalPrice.toFixed(2) + " отправлен на обработку";
+            alert(message);
+        });
         $('.getPrice').on('click', function(){
             var price = $(this).closest('.color-block').find('.slider').data('price');
             var amount = $(this).closest('.color-block').find('#amount').val();
-            var totalPrice;
-            console.dir(amount);
-            if (amount >= 10 ) {
-                totalPrice = price * amount * 0.5;
-            }
-            else if ( amount >= 3 ) {
-                totalPrice = price * amount * 0.7;
-            }
-            else if ( amount >= 1 ) {
-                totalPrice = price * amount;
-            }
-            else if ( amount == 0 ) {
-                totalPrice = price;
-            };
+            var totalPrice = getPrice(price, amount);
             var message = $('<br>Это будет стоить ' + totalPrice.toFixed(2) + '₴</br>');
             $(this).closest('.slider').append(message);
             $(this).prev().prev().remove();
@@ -28,3 +23,20 @@
             $(this).remove();
         });
     });
+
+function getPrice(price,amount) {
+        var totalPrice = 0;
+        if (amount >= 10 ) {
+            totalPrice = price * amount * 0.5;
+        }
+        else if ( amount >= 3 ) {
+            totalPrice = price * amount * 0.7;
+        }
+        else if ( amount >= 1 ) {
+            totalPrice = price * amount;
+        }
+        else if ( amount == 0 ) {
+            totalPrice = price;
+        };
+        return totalPrice;
+    }
