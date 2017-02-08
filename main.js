eee9c08cb0014b3939fa78a9f7753a551c6f0906
Flippylolz <assasin463@gmail.com>
2017-02-08 03:46:37 +0200

    new button added

--- main.js
+++ main.js
@@ -4,4 +4,12 @@
         {
             $(this).closest('.color-block').children().last().slideToggle("slow");
         });
+        $('.getPrice').on('click', function(){
+            var block = $(this).closest('.slider');//.children().last().prev();
+            var price = $(this).closest('.color-block').children().first().next().data('price');
+            var message = $('<span>Это будет стоить ' + price + ' гривен</span>');
+            block.append(message);
+            $(this).remove();
+            // console.dir(price);
+        });
     });
