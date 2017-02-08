d7708da38b03ccc5b101486297256b1c3e5aeea1
Flippylolz <assasin463@gmail.com>
2017-02-08 03:47:31 +0200

    hot fix

--- main.js
+++ main.js
@@ -5,11 +5,9 @@
             $(this).closest('.color-block').children().last().slideToggle("slow");
         });
         $('.getPrice').on('click', function(){
-            var block = $(this).closest('.slider');//.children().last().prev();
             var price = $(this).closest('.color-block').children().first().next().data('price');
             var message = $('<span>Это будет стоить ' + price + ' гривен</span>');
-            block.append(message);
+            $(this).closest('.slider').append(message);
             $(this).remove();
-            // console.dir(price);
         });
     });
