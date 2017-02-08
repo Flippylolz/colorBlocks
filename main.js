a1b514ad4d953b396008750615d375e14a8a4c93
Flippylolz <assasin463@gmail.com>
2017-02-08 18:29:35 +0200

    almost done

--- main.js
+++ main.js
@@ -5,9 +5,13 @@
             $(this).closest('.color-block').children().last().slideToggle("slow");
         });
         $('.getPrice').on('click', function(){
-            var price = $(this).closest('.color-block').children().first().next().data('price');
-            var message = $('<span>Это будет стоить ' + price + ' гривен</span>');
+            var price = $(this).closest('.color-block').find('.slider').data('price');
+            var amount = $(this).closest('.color-block').find('#amount').val();
+            var totalPrice = price * amount;
+            if (totalPrice == 0) {totalPrice = price }
+            var message = $('<br>Это будет стоить ' + totalPrice + ' гривен</br>');
             $(this).closest('.slider').append(message);
             $(this).remove();
+            $(this).prev().remove();//не работает
         });
     });
