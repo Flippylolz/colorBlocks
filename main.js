21120d88974557d07b203c3e65ef423519439887
Flippylolz <assasin463@gmail.com>
2017-02-08 18:55:48 +0200

    discount payment

--- main.js
+++ main.js
@@ -7,9 +7,18 @@
         $('.getPrice').on('click', function(){
             var price = $(this).closest('.color-block').find('.slider').data('price');
             var amount = $(this).closest('.color-block').find('#amount').val();
-            var totalPrice = price * amount;
-            if (totalPrice == 0) {totalPrice = price }
-            var message = $('<br>Это будет стоить ' + totalPrice + ' гривен</br>');
+            var totalPrice;
+            console.dir(amount);
+            if (amount >= 10 ) {
+                totalPrice = price * amount * 0.5;
+            }
+            else if ( amount >= 3 ) {
+                totalPrice = price * amount * 0.7;
+            }
+            else if ( amount == 0 ) {
+                totalPrice = price;
+            };
+            var message = $('<br>Это будет стоить ' + totalPrice.toFixed(2) + '₴</br>');
             $(this).closest('.slider').append(message);
             $(this).remove();
             $(this).prev().remove();//не работает
